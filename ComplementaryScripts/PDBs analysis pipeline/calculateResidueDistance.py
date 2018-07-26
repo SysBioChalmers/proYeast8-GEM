bca899f38c088fcd6e2bfdd0733b1404e084aeed
hongzhonglu <luho@chalmers.se>
2018-07-26 18:45:38 +0200

    fix: refine the code to calculate the redsidue distance and parsing the vcf file

--- ComplementaryScripts/PDBs analysis pipeline/calculateResidueDistance.py
+++ ComplementaryScripts/PDBs analysis pipeline/calculateResidueDistance.py
@@ -1,21 +1,19 @@
-'''Function to calculate the distance between two residues in a protein structure
-Hongzhong Lu 2018-07-17'''
-
-#package
-
 from Bio.PDB import *
 import os    ##for directory
 import numpy
+import pandas as pd
+#set the directory
+os.chdir('/Users/luho/PycharmProjects/python learning/venv/project2_pdb/code')
+print (os.getcwd()) #obtain the present directory
 
-# function
+#function
 def calc_residue_dist(residue_one, residue_two):
     """Returns the C-alpha distance between two residues"""
     ''' an example
     residue_one = residue1
     residue_two = residue2'''
-    diff_vector = residue_one["CA"].coord - residue_two["CA"].coord
+    diff_vector = residue_one["CA"].coord - residue_two["CA"].coord ##some errors: KeyError: 'CA'
     return numpy.sqrt(numpy.sum(diff_vector * diff_vector))
-
 
 def calc_dist_matrix(chain_one, chain_two) :
     """Returns a matrix of C-alpha distances between two chains"""
@@ -30,44 +28,94 @@ def calc_dist_matrix(chain_one, chain_two) :
     return answer
 
 
-# main
-# set the directory
-os.chdir('/Users/luho/PycharmProjects/python learning/venv/project2_pdb/code')
-print (os.getcwd()) #obtain the present directory
-
-# input an example
+#get the paired distance
 p = PDBParser()
-structure = p.get_structure('1n8p', '../data/1n8p.pdb')
+gene = 'YBR115C'
+infile = '../data/' + gene + '.pdb'
+outfile = '/Users/luho/Google Drive/R application and code/protein 3D structure QC and QA/SNP analysis pipeline/data/ResidueDistance_' + gene + '.xlsx'
 
-for model in structure:
-    for chain in model:
-        for residue in chain:
-            for atom in residue:
-                print(atom)
-
-
-# Iterate over all atoms in a structure
-for atom in structure.get_atoms():
-    print(atom)
-
-# Iterate over all residues in a model
-for residue in model.get_residues():
-    print(residue)
-
+structure = p.get_structure(gene, infile)
 model = structure[0]
 chain = model['A']
-residue = chain[100]
-atom = residue['CA']
+ss = calc_dist_matrix(chain,chain)
+ResidueDistance = pd.DataFrame(ss)
 
+
+#get sequence
+from Bio.PDB.PDBParser import PDBParser
+from Bio.PDB.Polypeptide import PPBuilder
+ppb=PPBuilder()
+seq = []
+for pp in ppb.build_peptides(structure):
+    seq.append(pp.get_sequence())
+    print(pp.get_sequence())
+
+ResidueDistance.columns = list(seq[0]) #sometimes the seq number is not equal to the distance number
+ResidueDistance['row'] = list(seq[0])
+ResidueDistance0 = ResidueDistance.set_index('row')
+
+writer = pd.ExcelWriter(outfile)
+ResidueDistance0.to_excel(writer,'Sheet1')
+writer.save()
+
+
+
+
+
+
+
+
+
+
+
+'''code to check why there exist error for protein structure YIL160C'''
+lines = open('../data/YIL160C.pdb', 'r').readlines()
+index1 = []
+aa = []
+for line in lines:
+    column = line.split(' ')
+    column0 = [x for i, x in enumerate(column) if x !='']
+    print(column0)
+    if 'CA' in column0[2]:
+        index1.append(column0[5])
+        aa.append(column0[3])
+
+import numpy as np
+[i for i, x in enumerate(index1) if x =='417']
+s1 = index1[0:390]
+s2 = [str(x) for x in range(25,418)]
+x = np.array(s1)
+y = np.array(s2)
+np.setdiff1d(y,x)
+
+
+s3 = index1[390:]
+z = np.array(s3)
+np.setdiff1d(z,y)
+
+ResidueDistance.columns = list(seq[2]) #sometimes the seq number is not equal to the distance number
+ResidueDistance['row'] = list(seq[2])
+
+
+
+
+
+'''example to calculate the distance'''
+#residues = structure.get_residues()
+# Iterate over all atoms in a structure
+#for atom in structure.get_atoms():
+#    print(atom)
+
+# Iterate over all residues in a model
+#for residue in model.get_residues():
+#    print(residue)
+
+
+#residue = chain[100]
+#atom = residue['CA']
 #calculate the distance
-residue1 = chain[99]
-residue2 = chain[101]
-distance = residue1['CA'] - residue2['CA']
+#residue1 = chain[99]
+#residue2 = chain[101]
+#distance = residue1['CA'] - residue2['CA']
 
 #use the function to get the distance between any two residues
-ss = calc_dist_matrix(chain,chain)
-print(ss)
-
-
-
-
---
+++ ComplementaryScripts/PDBs analysis pipeline/vcfParse_1011.py
@@ -0,0 +1,103 @@
+import os    ##for directory
+import pandas as pd
+
+# set the directory
+os.chdir('/Users/luho/PycharmProjects/python learning/venv/project2_pdb/data')
+print (os.getcwd()) #obtain the present directory
+
+#first step
+#fiter the snp from vcf file
+#find snp insertion deletion in vcf file
+lines = open('1011Matrix.gvcf', 'r').readlines()
+out = open('snp','w')
+out1 = open('insertion','w')
+out2 = open('deletion','w')
+flag1 = 0
+flag2 = 0
+flag3 = 0
+for line in lines:
+    if line[0] == '#':
+        continue
+    column = line.split('\t')
+    alt = column[4]
+    ref = column[3]
+    if len(ref) ==1 and alt == 'A':
+        out.write(line)
+        flag1 +=1
+    elif len(ref) ==1 and alt == 'T':
+        out.write(line)
+        flag1 += 1
+    elif len(ref) ==1 and alt == 'C':
+        out.write(line)
+        flag1 += 1
+    elif len(ref) == 1 and alt == 'G':
+        out.write(line)
+        flag1 += 1
+    elif ',' in alt: # this should be changed into: elif len(ref)==1 and ',' in alt
+        out.write(line)
+        flag1 += 1
+    elif len(alt) > 1 and ',' not in alt:
+        out1.write(line)
+        flag2 += 1
+    elif len(ref) >1:
+        out2.write(line)
+        flag3 += 1
+
+print('SNPs is:',flag1)
+print('Insertion is:', flag2)
+print('Deletion is:', flag3)
+out.close()
+out1.close()
+out2.close()
+
+#second step
+#input the SNP file
+#output file will be the SNP under each sample
+#filter sample based on the GT
+SNPs = open('snp_strain_name', 'r').readlines()
+header0 = SNPs[0].split('\t')
+strain_name0 = header0[9:]
+
+def getHomoSample(sample_all, common_row, strain_name=strain_name0):
+
+    index_new = []
+    for i, x in enumerate(sample_all):
+        if '1/1' in x:
+            index_new.append(i)
+    sample_all1 = pd.Series(sample_all)
+    strain_name1 = pd.Series(strain_name)
+
+    parameter_new = sample_all1[index_new]
+    sample_new = strain_name1[index_new]
+
+    parameter = parameter_new.tolist()
+    sample = sample_new.tolist()
+
+    commom_row1 = '\t'.join(common_row)
+    newRecord = []
+    for i in range(len(sample)):
+        record = sample[i] +'\t'+ parameter[i] + '\t' + commom_row1
+        newRecord.append(record)
+    return newRecord
+
+#read and establish a new file to store the result
+SNPs = open('snp', 'r').readlines()
+out3 = open('SNP_target','w')
+
+for snp in SNPs:
+    snp0 = snp.split('\t')
+    common_row2 = snp0[0:9]
+    sample_all2 = snp0[9:]
+    ss = getHomoSample(sample_all2, common_row2) # loop based on row and then should choose which column be used
+    print(ss)
+    if ss ==[]:
+        continue
+    else:
+        for item in ss:
+            out3.write(item)
+            out3.write('\n')
+
+out3.close()
+
+
+#check the quality of the SNP_target
