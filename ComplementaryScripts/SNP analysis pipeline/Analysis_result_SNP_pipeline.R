5004e1a98cda9470fa69e9bff9ce8e5ae102a68b
hongzhonglu <luho@chalmers.se>
2018-11-20 15:45:59 +0100

    fix: refine the code for the strain classification based on SNP data

--- ComplementaryScripts/SNP analysis pipeline/Analysis_result_SNP_pipeline.R
+++ ComplementaryScripts/SNP analysis pipeline/Analysis_result_SNP_pipeline.R
@@ -9,14 +9,21 @@ source("preprocess_1011_project_function.R")
 source("genomics annotation summary.R")
 
 # step0 choose samples that need to be analyzed
-strain_classification <- read_excel("data/strain_classification.xls")
-strain_type <- "bioethanol"
-strain_select1 <- chooseStrain(type = strain_type)
+#strain_classification <- read_excel("data/strain_classification.xls")
+#strain_type <- "bioethanol"
+#strain_select1 <- chooseStrain(type = strain_type)
+strain_classification <- read.table("data/strain_glycerol_classification.txt", header = TRUE, stringsAsFactors = FALSE)
+strain_classification <- strain_classification[, c('strain_name','type')]
+strain_type <-"glycerol_high"
+strain_select1 <- chooseStrain(type = "glycerol_high")
+
+
+
 
 # input the data of clumps method
-clumps_ex <- read.table(paste('result0/CLUMPS from pdb_ex for ',strain_type, '/pdb_EX.txt',sep = ""), header= TRUE, sep = "\t", stringsAsFactors = FALSE)
+clumps_ex <- read.table(paste('result/CLUMPS from pdb_ex for ',strain_type, '/pdb_EX.txt',sep = ""), header= TRUE, sep = "\t", stringsAsFactors = FALSE)
 clumps_ex$pdb_source <- "Exp"
-clumps_homo <- read.table(paste('result0/CLUMPS from pdb_homo for ',strain_type, '/pdb_info.txt',sep = ""), header= TRUE, sep = "\t", stringsAsFactors = FALSE)
+clumps_homo <- read.table(paste('result/CLUMPS from pdb_homo for ',strain_type, '/pdb_info.txt',sep = ""), header= TRUE, sep = "\t", stringsAsFactors = FALSE)
 clumps_homo$pdb_source <- "Homo"
 clumps_all <- rbind.data.frame(clumps_ex, clumps_homo)
 clumps_all_fiter <- filter(clumps_all, p_value <= 0.05)
@@ -24,6 +31,11 @@ clumps_all_fiter <- filter(clumps_all, p_value <= 0.05)
 # remove pdb file with the same gene and same residue coordinate
 clumps_all_fiter$combineINF <- paste(clumps_all_fiter$locus, clumps_all_fiter$sstart2, clumps_all_fiter$send2, sep = "@@")
 clumps_all_fiter0 <- clumps_all_fiter[!duplicated(clumps_all_fiter$combineINF),]
+
+# merge with the genome annotation for the the interesting protein set
+gene_annotation <- read.delim2('data/all_gene_yeast with annotation from different database.txt', header = TRUE, stringsAsFactors = FALSE)
+clumps_all_fiter0 <- merge.data.frame(clumps_all_fiter0,gene_annotation,by.x = "locus", by.y = 'gene', all.x = TRUE)
+
 
 # gene with pdb file which have mutation enrichment
 gene_sum <- unique(clumps_all_fiter0$locus)
@@ -40,7 +52,8 @@ gene_sum0 <- paste0(gene_sum,collapse = ",")
 # this lead to a new question? whether the choosed pdb file could
 # represent the aimed protein
 # in general, we should choose the PDB file contains the longest residue sequence
-geneset1 <- c('YML100W','YAR035W','YJL068C','YMR246W')
+#geneset1 <- c('YML100W','YAR035W','YJL068C','YMR246W') #for bioethonal
+geneset1 <- 'YHL032C'
 geneset1_index <- which(clumps_all_fiter0$locus %in% geneset1)
 geneset1_detail <- clumps_all_fiter0[geneset1_index,]
 
@@ -48,6 +61,7 @@ for (i in 1:length(geneset1_detail$locus)) {
   print(i)
   # step 1
   # preprocess the SNP information
+  i=1
   ss <- geneset1_detail$locus[i]
   mutated_gene0 <- preprocessSNP(ss)
   mutated_gene1 <- mutated_gene0[which(mutated_gene0$strain %in% strain_select1$Standardized_name), ]
@@ -89,6 +103,8 @@ for (i in 1:length(geneset1_detail$locus)) {
   result$pdbID <- geneset1_detail$pdbid[i]
   result <- select(result, orf, ref, position, alt, Freq, pdbID)
   print(result)
+  write.table(result, "result/gene_mutation_related_to_glycerol.txt", row.names = FALSE, sep = " ")
+  
 }
 
 # save the results in format which can predict the protein function
--- ComplementaryScripts/SNP analysis pipeline/classification_strain_based_snp.R
+++ ComplementaryScripts/SNP analysis pipeline/classification_strain_based_snp.R
@@ -9,10 +9,10 @@ source("preprocess_1011_project_function.R")
 source("genomics annotation summary.R")
 
 
-
 # function to preprocess the SNP information
 # this function will obtain all the mutated residue information for one protein
 printAllMutationWithGene <- function(ss){
+  #example
   #ss <- 'YMR246W'
   mutated_gene0 <- preprocessSNP(ss)
   mutated_gene1 <- mutated_gene0[which(mutated_gene0$strain %in% strain_select1$Standardized_name), ]
@@ -40,9 +40,11 @@ printAllMutationWithGene <- function(ss){
 # datainput
 # choose samples that need to be analyzed
 strain_classification <- read_excel("data/strain_classification.xls")
-growth_phenotype <- read.table('data/strain_classification based on relative growth under high temperature.txt',header = TRUE, stringsAsFactors = FALSE)
+# input the relative growht rate under 42°C
+growth_phenotype <- read.table('data/strain_classification based on relative growth under high temperature.txt',header = TRUE, stringsAsFactors = FALSE) 
 strain_type <- "all_strain"
-strain_select1 <- chooseStrain(type = strain_type)
+strain_select1 <- strain_classification[,'Standardized_name']
+
 # choose the interested protein and the related mutation datasets
 gene_mutation  <- read_excel("data/gene_mutation_related_to_heat.xlsx")
 gene_mutation0 <- gene_mutation  %>% unite(., mutation_result, gene, alt, position, sep = "@@") %>%
@@ -61,7 +63,8 @@ p_YAR035W <- printAllMutationWithGene('YAR035W')
 p_YML100W <- printAllMutationWithGene('YML100W')
 # YJL068C
 p_YJL068C <- printAllMutationWithGene('YJL068C')
-  
+ 
+ 
 #merge the above data
 strain_mutation <- rbind.data.frame(p_YMR246W,p_YAR035W,p_YML100W,p_YJL068C)
 mutation_summary <- data.frame(strain_name=unique(strain_mutation$strain), stringsAsFactors = FALSE) 
@@ -96,68 +99,55 @@ for(i in gene_mutation1){
 
 
 mutation_matrix <- select(mutation_summary, c('strain_name',gene_mutation1 ))
+
+
+
 #strain classification based on several genes with a series mutations
 #here we can classify the strain based on cluster 
 #and the total numer of mutation occured in each strain
-
-
-#method1 cluster the strain based on the maxrix
-#cluster analysis
-library(ggfortify)
-mutation_summary$type <- getSingleReactionFormula(strain_classification$Clades,strain_classification$Standardized_name,mutation_summary$strain_name)
-df <- select(mutation_summary, c(gene_mutation1))
-name0 <- paste('s',1:ll0,sep = "")
-colnames(df) <- name0
-autoplot(prcomp(df), data = mutation_summary, colour = 'type')
-#obtain the cluster
-km <- kmeans(df, 3)
-mutation_matrix$cluster <- km$cluster
-growth_all <- growth_phenotype$growth
-
-strain1 <- mutation_matrix[mutation_matrix$cluster==1,] %>% select(.,strain_name)
-cluster1 <-  growth_phenotype[growth_phenotype$strain_name %in% strain1$strain_name,]
-
-strain2 <- mutation_matrix[mutation_matrix$cluster==2,] %>% select(.,strain_name)
-cluster2 <-  growth_phenotype[growth_phenotype$strain_name %in% strain2$strain_name,]
-
-strain3 <- mutation_matrix[mutation_matrix$cluster==3,] %>% select(.,strain_name)
-cluster3 <-  growth_phenotype[growth_phenotype$strain_name %in% strain3$strain_name,]
-#plot the result
-x1 <- cluster1$growth
-x2 <- cluster2$growth
-x3 <- cluster3$growth
-boxplot(x1,x2,x3) 
-t.test(x1,x3)
-t.test(x1,x2)
-t.test(x2,x3)
-
-
-
-#method2 cluster the strain based on the mutation number
-##based on number
 mutation_matrix0 <- mutation_matrix
 mutation_matrix0$sum <- rowSums(mutation_matrix0[,2:13])
+
+# statical analysis for multiple mutations from several genes 
 table(mutation_matrix0$sum)
-plot(density(mutation_matrix0$sum))
+#mutation_matrix0$sum <- as.factor(mutation_matrix0$sum)
+#ggplot(mutation_matrix0, aes(sum)) + geom_bar(fill = "#FF6666") +
+#  xlab('Mutation number') + ylab('Strain number') +
+#  theme_minimal()
+
+
+# classification all strains in three type
 strain1 <- mutation_matrix0[mutation_matrix0$sum >= 6,] %>% select(.,strain_name)
 cluster1 <-  growth_phenotype[growth_phenotype$strain_name %in% strain1$strain_name,]
+cluster1$type <- 'g1'
 
 strain2 <- mutation_matrix0[mutation_matrix0$sum >= 2 & mutation_matrix0$sum <= 5,] %>% select(.,strain_name)
 cluster2 <-  growth_phenotype[growth_phenotype$strain_name %in% strain2$strain_name,]
-
+cluster2$type <- 'g2'
 
 strain3 <- mutation_matrix0[mutation_matrix0$sum <=1,] %>% select(.,strain_name)
 cluster3 <-  growth_phenotype[growth_phenotype$strain_name %in% strain3$strain_name,]
+cluster3$type <- 'g3'
 
+# statistical analysis
 x1 <- cluster1$growth
 x2 <- cluster2$growth
 x3 <- cluster3$growth
 
-##The plots
-boxplot(x1,x2,x3)
 t.test(x1, x3)  
 t.test(x1, x2) 
 t.test(x2, x3) 
+
+wilcox.test(x1, x3, alternative = "two.sided")
+wilcox.test(x1, x2, alternative = "two.sided")
+wilcox.test(x2, x3, alternative = "two.sided")
+
+#plot the result
+strain_classification0 <- rbind.data.frame(cluster1, cluster2, cluster3)
+boxplot(growth~type,data=strain_classification0, main="", col=terrain.colors(4),
+        xlab="Strain classification", ylab="Relative growth rate",
+        ylim=c(0,1), frame=FALSE,cex.lab=1.4, cex.axis=1.2)
+
 
 
 # strain classification based on two mutations of  'YML100W'
@@ -165,27 +155,38 @@ gene_mutation2 <- gene_mutation1[str_detect(gene_mutation1,'YML100W')]
 mutation_matrix02 <- select(mutation_summary, c('strain_name',gene_mutation2 ))
 mutation_matrix02$sum <- rowSums(mutation_matrix02[,2:3])
 
+# classification all strains in three type
 strain1 <- mutation_matrix02[mutation_matrix02$sum >= 2,] %>% select(.,strain_name)
 cluster1 <-  growth_phenotype[growth_phenotype$strain_name %in% strain1$strain_name,]
-
+cluster1$type <- 's1'
 strain2 <- mutation_matrix02[mutation_matrix02$sum ==1,] %>% select(.,strain_name)
 cluster2 <-  growth_phenotype[growth_phenotype$strain_name %in% strain2$strain_name,]
-
+cluster2$type <- 's2'
 strain3 <- mutation_matrix02[mutation_matrix02$sum <=0,] %>% select(.,strain_name)
 cluster3 <-  growth_phenotype[growth_phenotype$strain_name %in% strain3$strain_name,]
+cluster3$type <- 's3'
 
+
+# statistical analysis
 x1 <- cluster1$growth
 x2 <- cluster2$growth
-summary(x2)
 x3 <- cluster3$growth
 
+t.test(x1, x3)  
+t.test(x1, x2) 
+t.test(x2, x3) 
 
-##The plots
-boxplot(x1,x2, x3)
-t.test(x1,x3) 
-t.test(x2,x3) 
-t.test(x1,x2) 
+wilcox.test(x1, x3, alternative = "two.sided")
+wilcox.test(x1, x2, alternative = "two.sided")
+wilcox.test(x2, x3, alternative = "two.sided")
 
+
+
+#plot the result
+strain_classification0 <- rbind.data.frame(cluster1, cluster2, cluster3)
+boxplot(growth~type,data=strain_classification0, main="", col=terrain.colors(4),
+        xlab="Strain classification", ylab="Relative growth rate",
+        ylim=c(0,1), frame=FALSE, cex.lab=1.4, cex.axis=1.2)
 
 
 # further check the growth phenotype for the bioethonal strains
