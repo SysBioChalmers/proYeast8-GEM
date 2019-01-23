a4431bf3a8e998d1df896fadced5888efae79af2
hongzhonglu <luho@chalmers.se>
2019-01-23 14:47:43 +0100

    fix: refine the process for the hotspot analysis, add the code for the main result in manuscript

---
+++ ComplementaryScripts/SNP analysis pipeline/classification_strain_based_snp for 8 carbon source_general.R
@@ -0,0 +1,183 @@
+#----------------note
+# this main script is used to classify the strain based on residues mutation information obtained
+# from CLUMPS analysis pipeline.
+# firstly we obtain the residues mutation of one gene for all strains
+# then we can classify the strains based on the specific residue mutations
+source("Analysis_result_SNP_pipeline_hotspot.R")
+
+
+# function to preprocess the SNP information
+# this function will obtain all the mutated residue information for one protein
+printAllMutationWithGene <- function(ss){
+  #ss <- 'YMR246W'
+  mutated_gene0 <- preprocessSNP(ss)
+  mutated_gene1 <- mutated_gene0[which(mutated_gene0$strain %in% strain_select1$Standardized_name), ]
+  gene_snp <- getGeneCoordinate(gene_name = ss, genesum = gene_feature_GEM)
+  # input the protein coordinate ID
+  p1 <- 1
+  p2 <- length(gene_snp[['protein']])
+  p3 <- paste(p1, p2, sep = "-")
+  # produce the mutaed residue and its position
+  pos_residue1 <- list()
+  for (j in seq_along(mutated_gene1$strain)) {
+    print(j)
+    pos_residue1[[j]] <- PositionResidueSNP(mutated_gene1$Pos[j], mutated_gene1$Alt[j], ss)
+    mutated_gene1$mutation_result[j] <- pos_residue1[[j]][1]
+  }
+  
+  result <- mutated_gene1[!is.na(mutated_gene1$mutation_result),]
+  result$gene_mutation <- paste(result$Gene2,result$mutation_result, sep = '@@')
+  result0 <- select(result,strain,gene_mutation)
+  return(result0)
+}
+
+
+# part 3
+# datainput
+# choose samples that need to be analyzed
+growth_phenotype <- read_excel("data/phenoMatrix_35ConditionsNormalizedByYPD.xlsx") 
+growth_phenotype <- select(growth_phenotype, strain_name, YPGLYCEROL)
+colnames(growth_phenotype) <- c('strain_name','growth')
+
+# here we choose strain with the related specific phenotype but remove the strains
+# which have been used for finding interesting proteins list
+infile1 = paste("data/strain_", substrate0,"_classification.txt", sep = "")
+strain_choosed <- read.table(infile1, header = TRUE, stringsAsFactors = FALSE)
+strain_classification <- read_excel("data/strain_classification.xls")
+strain_select1 <- strain_classification[which(strain_classification$Standardized_name %in% strain_choosed$strain_name ==FALSE),]
+
+
+
+
+# part 4
+# choose the interested protein and the related mutation datasets
+gene_mutation  <- read.table("result/gene_mutation_related_to_growth.txt", header = TRUE, stringsAsFactors = FALSE)
+gene_mutation0 <- gene_mutation  %>% unite(., mutation_result, orf, alt, position, sep = "@@") %>%
+   filter(.,Freq >=1)
+gene_mutation1 <- gene_mutation0$mutation_result
+ll0 <- length(gene_mutation1)
+
+# produce the gene mutation matrix for all the strains
+# YHL032C
+p <- printAllMutationWithGene(gene_mutation$orf[1])
+# YAR035W 
+#p_YAR035W <- printAllMutationWithGene('YAR035W')
+# YML100W
+#p_YML100W <- printAllMutationWithGene('YML100W')
+# YJL068C
+#p_YJL068C <- printAllMutationWithGene('YJL068C')
+
+#merge the above data
+strain_mutation <- rbind.data.frame(p)
+mutation_summary <- data.frame(strain_name=unique(strain_mutation$strain), stringsAsFactors = FALSE) 
+mutation_summary$mutation_result <- getMultipleReactionFormula(strain_mutation$gene_mutation,strain_mutation$strain,mutation_summary$strain_name)
+
+
+#establish a list contains the strain name and mutation 
+#here we should produce a matrix to check whether each mutation contains in each strain,
+#function to check whether a mutation existed
+checkExist <- function(mutate00, mutation_list=mutation_summary){
+s1 <- str_split(mutation_list$mutation_result,';')
+exist0 <- vector()
+for(i in seq_along(s1)){
+  s11 <- s1[[i]]
+   if(mutate00 %in% s11){
+      exist0[i] <- 1
+   } else {
+     exist0[i] <- 0
+  }
+ }
+  return(exist0)
+} 
+
+
+for(i in gene_mutation1){
+  print(i)
+  checkExist(i)
+  mutation_summary[,i] <- checkExist(i, mutation_list=mutation_summary)
+}
+
+
+mutation_matrix <- select(mutation_summary, c('strain_name',gene_mutation1 ))
+#strain classification based on several genes with a series mutations
+#here we can classify the strain based on cluster 
+#and the total numer of mutation occured in each strain
+
+
+#method2 cluster the strain based on the mutation number
+##mutation number analysis
+mutation_matrix0 <- mutation_matrix
+mutation_matrix0$sum <- rowSums(mutation_matrix0[,2:ncol(mutation_matrix0)]) #calculate the number of aimed SNP
+table(mutation_matrix0$sum)
+plot(density(mutation_matrix0$sum))
+
+
+
+# part5
+# classification based on number
+strain1 <- mutation_matrix0[mutation_matrix0$sum >= 2,] %>% select(.,strain_name)
+cluster1 <-  growth_phenotype[growth_phenotype$strain_name %in% strain1$strain_name,]
+cluster1$type <- 'g1'
+
+strain2 <- mutation_matrix0[mutation_matrix0$sum >= 0 & mutation_matrix0$sum <= 1,] %>% select(.,strain_name)
+cluster2 <-  growth_phenotype[growth_phenotype$strain_name %in% strain2$strain_name,]
+cluster2$type <- 'g2'
+
+strain3 <- mutation_matrix0[mutation_matrix0$sum <=1,] %>% select(.,strain_name)
+cluster3 <-  growth_phenotype[growth_phenotype$strain_name %in% strain3$strain_name,]
+cluster3$type <- 'g3'   #'SACE_YDK' not in growth_phenotype
+
+
+
+# statistical analysis
+x1 <- cluster1$growth
+x2 <- cluster2$growth
+x3 <- cluster3$growth
+
+#test analysis
+t.test(x1, x3)  
+t.test(x1, x2) 
+t.test(x2, x3)
+
+wilcox.test(x1, x3, alternative = "two.sided")
+wilcox.test(x1, x2, alternative = "two.sided")
+wilcox.test(x2, x3, alternative = "two.sided")
+
+
+## good plot
+strain_classification0 <- rbind.data.frame(cluster1, cluster2, cluster3)
+
+# method1
+p <- ggplot(strain_classification0, aes(x=type,y=growth, fill=type))
+p + geom_boxplot(outlier.size=0) +  
+   geom_jitter(position=position_jitter(h=.1), aes(color = type), alpha=0.3)
+
+# method2
+p <-  ggplot(strain_classification0, aes( x = type, y = growth, fill = type))
+p + geom_boxplot()
+
+
+# plot the result
+boxplot(growth~type,data=strain_classification0, main="", col=terrain.colors(4),
+        xlab="Mutation classification", ylab="Relative growth rate")
+
+
+
+# Add data points
+strain_classification0$type <- as.factor(strain_classification0$type)
+mylevels<-levels(strain_classification0$type)
+levelProportions<-summary(strain_classification0$type)/nrow(strain_classification0)
+
+for(i in 1:length(mylevels)){
+  thislevel<-mylevels[i]
+  thisvalues<- unlist(strain_classification0[strain_classification0$type==thislevel, "growth"],use.names = FALSE)
+  
+  # take the x-axis indices and add a jitter, proportional to the N in each level
+  myjitter<-jitter(rep(i, length(thisvalues)), amount=levelProportions[i]/2)
+  points(myjitter, thisvalues, pch=20, col=rgb(0,0,0,.2)) 
+}
+
+
+
+
+
--- ComplementaryScripts/SNP analysis pipeline/preprocess_1011_project_hot_spot_v3(exp)_glycerol.R
+++ ComplementaryScripts/SNP analysis pipeline/preprocess_1011_project_hot_spot_v3(exp)_glycerol.R
@@ -9,106 +9,38 @@ source("preprocess_1011_project_function.R")
 
 
 # step0 choose samples that need to be analyzed
-strain_classification <- read_excel("data/strain_classification.xls")
-unique(strain_classification$Clades)
-strain_type <- "Wine"
-strain_select1 <- filter(strain_classification, str_detect(strain_classification$Clades, strain_type)) %>%
-  select(., Standardized_name)
+#strain_classification <- read_excel("data/strain_classification.xls")
+#unique(strain_classification$Clades)
+#strain_type <- "Wine"
+#strain_select1 <- filter(strain_classification, str_detect(strain_classification$Clades, strain_type)) %>%
+#  select(., Standardized_name)
 #strain_type <- "all_strain"
 #strain_select1 <- chooseStrain(type = strain_type)
 
 
+# part 0
+substrate0 <- 'glycerol'
+strain_type0 <- 'glycerol_high'
+infile0 = paste("data/strain_", substrate0,"_classification.txt", sep = "")
+strain_classification <- read.table(infile0, header = TRUE, stringsAsFactors = FALSE)
+strain_classification <- strain_classification[, c("strain_name", "type")]
 
-
-
-#------------single sample--------------------------------------------------------
-#------------new version----------------------------------------------------------
-#------------this version is used to preprocess data from 1011 project
-# step 1
-# preprocess the SNP information
-ss <- "YAL012W"
-mutated_gene0 <- preprocessSNP(ss)
-mutated_gene1 <- mutated_gene0[which(mutated_gene0$strain %in% strain_select1$Standardized_name), ]
-
-gene_snp <- getGeneCoordinate(gene_name = ss, genesum = gene_feature_GEM)
-gene_snp[["pro_mutation_count"]] <- countMutationProtein(gene_name = ss, mutation_annotation = mutated_gene1)
-sum(gene_snp[["pro_mutation_count"]])
-gene_snp[["pro_coordinate"]] <- 1:length(gene_snp[["protein"]])
-pos_mutation <- which(gene_snp[["pro_mutation_count"]] != 0)
-
-
-# step 2 input the structure information
-# input the distance of all the pired residues
-pdbID <- "1n8p@A"
-r1 <- 1
-r2 <- 387 # input the corrected residue sequnence
-r3 <- paste(r1, r2, sep = "-")
-dirForDistanceMatrix <- paste("residue_distance/pdb_ex/", pdbID, ".txt", sep = "")
-ResidueDistance0 <- read.table(dirForDistanceMatrix, sep = ",") # in the followed calculation, the matrix dosen't have the col and row names
-ResidueDistance0 <- as.matrix(ResidueDistance0)
-dim(ResidueDistance0)
-ResidueDistance <- ResidueDistance0[r1:r2, r1:r2]
-
-
-# the amino acid sequence in structure is from 2:394 while  the original sequence is from 1:394
-# obtain the mutation information for the structure
-p1 <- 2
-p2 <- 388
-p3 <- paste(p1, p2, sep = "-")
-seq_3D_origin <- p1:p2 # seq_from_3D <- 2:394 #"YAL012W.fasta"#this is the coordinated of original protein sequence and should changed into 3D structure coordinates
-amino_acid_3D <- gene_snp[["protein"]][seq_3D_origin]
-count_mutation_3D <- gene_snp[["pro_mutation_count"]][seq_3D_origin]
-
-# mutation position on structure and #mutation number on structure
-pos_mutation_3D <- which(count_mutation_3D != 0)
-seq_3D <- 1:length(count_mutation_3D) # seq0 is the coordinate of PDB structure
-mutation_count_3D <- count_mutation_3D[pos_mutation_3D]
-
-# wap calculation for each pair mutated residue
-# calculate the standardard sample number
-sample_standard1 <- sampleStand(count_mutation_3D)
-
-
-# step 3 hot spot analysis
-# this main function will be divided into different parts for easy understanding
-pos_residue1 <- list()
-for (i in seq_along(mutated_gene1$strain)) {
-  pos_residue1[[i]] <- PositionResidueSNP(mutated_gene1$Pos[i], mutated_gene1$Alt[i], ss)
+# function to choose the strain list with specific phenotype
+strainList <- function(substrate, strain_type) {
+  #input
+  #substrate <-'glycerol'
+  #strain_type: glycerol_high, glycerol_low, glycerol_medium. which represent three different types of 
+  #strains with high, medium and low growth rate on the medium with glycerol as the substrate
+  infile0 = paste("data/strain_", substrate,"_classification.txt", sep = "")
+  strain_classification <- read.table(infile0, header = TRUE, stringsAsFactors = FALSE)
+  strain_classification <- strain_classification[, c("strain_name", "type")]
+  # strain_type <-"glycerol_high"
+  strain_select0 <- chooseStrain(type = strain_type, strain0=strain_classification)
+  return( strain_select0)
 }
-
-
-pos_residue_df <- ResidueSum(pos_residue1)
-
-
-# mapping the mutate residue onto the original protein sequence
-gene_snp[["residue"]] <- getMultipleReactionFormula(pos_residue_df$residue, pos_residue_df$pos, gene_snp[["pro_coordinate"]])
-residue_3D <- gene_snp[["residue"]][seq_3D_origin]
-
-# obtain the paired residue
-residue_pair <- getHotVertice(aa_3d = seq_3D, residue0 = residue_3D, aa_pro = seq_3D_origin, distance0 = ResidueDistance)
-
-# calculate closeness of each cluster
-important_hot <- clusterAnalysis(residue_pair)
-
-
-# further calculate the p value of each choosed cluster
-# calculate the pvalue for each clust
-important_hot$pvalue <- getHotPvalue(
-  cluster0 = important_hot$cluster,
-  sample_standard = sample_standard1,
-  distance = ResidueDistance,
-  seq = seq_3D
-)
-
-# add sample information for the result
-important_hot$gene <- ss
-important_hot$seq_3D_origin <- p3
-important_hot$structure <- pdbID
-important_hot$seq_3D <- r3
-important_hot$stain_type <- strain_type
-outfile <- paste("result/hot_spot/", pdbID, "_", ss, ".txt", sep = "")
-write.table(important_hot, outfile, row.names = FALSE, sep = "\t")
-
+# prepare the strain
+strain_select1 <- strainList(substrate=substrate0, strain_type=strain_type0)
+strain_type <- strain_type0
 
 
 
